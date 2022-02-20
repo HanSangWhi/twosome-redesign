42eca96f12df2a1781d5d6f3b996b491409ac408
HanSangWhi <vlrmvka1234@gmail.com>
2022-02-20 17:58:34 +0900

    2월 20일 연습

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ const swiper = new Swiper(".swiper", {
     nextEl: '.swiper-button-next',
   },
 });
+
+// 왼쪽 메뉴 3개랑 가운데 그림 연결
+
+$(document).ready(function(){
+  $(".taps div").click(function(){
+    $(this).addClass("active")
+    $(this).siblings().removeClass("active");
+  })
+})
