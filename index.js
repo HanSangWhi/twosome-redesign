929b8d635bc68e7d4eb27dd9a379a3ef6306e913
HanSangWhi <vlrmvka1234@gmail.com>
2022-02-21 18:18:06 +0900

    연습

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".swiper-box1 .swiper", {
+  loop:true,
+  slidesPerView:4,
+  spaceBetween:0,
+  // 화살표
+  navigation: {
+    nextEl: '.swiper-button-next',
+  },
+});
+const swiper2 = new Swiper(".swiper-box2 .swiper", {
+  loop:true,
+  slidesPerView:4,
+  spaceBetween:0,
+  // 화살표
+  navigation: {
+    nextEl: '.swiper-button-next',
+  },
+});
+const swiper3 = new Swiper(".swiper-box3 .swiper", {
   loop:true,
   slidesPerView:4,
   spaceBetween:0,
@@ -11,8 +29,17 @@ const swiper = new Swiper(".swiper", {
 // 왼쪽 메뉴 3개랑 가운데 그림 연결
 
 $(document).ready(function(){
-  $(".taps div").click(function(){
-    $(this).addClass("active")
+  $(".banner-2 .con .taps>div").click(function(){
+    $(this).addClass("active");
     $(this).siblings().removeClass("active");
-  })
-})
+
+    var indexNum = $(this).index();
+    console.log(indexNum);
+
+    $(".banner-2 .con .swiper-con>div").eq(indexNum).addClass("active");
+    $(".banner-2 .con .swiper-con>div").eq(indexNum).siblings().removeClass("active");
+
+    // $(".banner-2 .con2 .menu-story>ul").eq(indexNum).addClass("active");
+    // $(".banner-2 .con2 .menu-story>ul").eq(indexNum).siblings().removeClass("active");
+  });
+});
