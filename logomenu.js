c9182e31ac346a07da17717db27b96caf4a4ed44
HanSangWhi <vlrmvka1234@gmail.com>
2022-03-14 18:57:12 +0900

    완성본 조금더 수정해야됨

---
+++ logomenu.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".header .con .top-bar-logo-right").click(function(){
+    $(".side-bar").addClass("active");
+  });
+  $(".side-bar .logo-btn").click(function(){
+    $(".side-bar").removeClass("active");
+  });
+  $(".header .con .top-bar-logo-right2").click(function(){
+    $(".side-bar").addClass("active");
+  });
+  $(".side-bar .logo-btn").click(function(){
+    $(".side-bar").removeClass("active");
+  });
+});
